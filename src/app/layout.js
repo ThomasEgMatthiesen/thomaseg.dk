c5c4f2767a3a03f14bbcdabd2163e9bbe971e7af
ThomasEgMatthiesen <kontakt@thomaseg.dk>
2026-04-23 12:09:10 +0200

    Refactor layout and styling: Update font to Noto Sans, adjust global styles, and enhance build manifest with error page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const sans = Noto_Sans({
+    variable: "--font-sans",
+    subsets: ["latin"],
 });
 
 export const metadata = {
@@ -18,10 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="da" className={`${sans.className} ${sans.variable} antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
